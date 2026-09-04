4e29502f8cf516e8afe145bf8697f9c51560c97d
ganga sagar tiwari <gangasagarg1114@gmail.com>
2026-09-04 12:24:58 +0530

    update code

--- songs-english.js
+++ songs-english.js
@@ -1,4 +1,4 @@
 // English songs playlist
 const englishSongs = [
-  { title: 'English Demo Track', artist: 'Sangeet Demo', src: 'songs/english/demo.mp3', cover: 'images/english/demo.jpg' }
+  { title: 'English Demo Track', artist: 'Sangeet Demo', src: 'songs/english/demo.mp3', cover: 'images/english/english.jpg' }
 ];
